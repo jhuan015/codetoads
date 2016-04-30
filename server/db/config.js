fa9610e1512f43631afb73f0c4224ad8e956c8f4
Jonathan Huang <jhuan015@ucr.edu>
2016-04-30 16:26:01 -0700

    (feat) Add Game export

--- server/db/config.js
+++ server/db/config.js
@@ -5,5 +5,7 @@ var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/codetodes';
 var connection = mongoose.connect(mongoURI);
 
 var User = require('./user/user');
+var Game = require ('./game/game');
 
 module.exports.User = User;
+module.exports.Game = Game;
