22033eece0577dd0466780f629debdfaab4190d0
Jonathan Huang <jhuan015@ucr.edu>
2016-05-01 15:54:07 -0700

    (feat) Update schemas and fix database name

--- server/db/user/user.js
+++ server/db/user/user.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 var relationship = require('mongoose-relationship');
 
 var userSchema = mongoose.Schema({
-  auth_id: { type: String, required: true },
+  auth_id: { type: String, unique: true, required: true },
   username: { type: String },
+  firstname: {type: String },
+  lastname: {type: String },
   winStreak: { type: Number },
   lostTo: { type: String },
   gamesPlayed: { type: mongoose.Schema.ObjectId, ref: 'Game' },
