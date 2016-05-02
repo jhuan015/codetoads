d64d32c30bbab237c8fb87c634c371779fbf456a
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-02 15:21:17 -0400

    add basic game page components

---
+++ client/app/jsx/game/answer.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Answer extends React.Component {
+  render (){
+    return (
+      <div>
+        Answer goes here.
+      </div>
+    )  
+  } 
+}
+
+module.exports = Answer
---
+++ client/app/jsx/game/game.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Game extends React.Component {
+  render (){
+    return (
+      <div>
+        Gaming box.
+      </div>
+    )  
+  } 
+}
+
+module.exports = Game
