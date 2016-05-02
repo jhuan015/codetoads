c326335ed5b44023a65fa7129ab74ef7e58bed23
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-02 15:19:33 -0400

    add basic react-router

--- client/app/jsx/boot.jsx
+++ client/app/jsx/boot.jsx
@@ -1,4 +1,21 @@
-ReactDOM.render(
-  <App clientId={AUTH0_CLIENT_ID} domain={AUTH0_DOMAIN} />,
-  document.getElementById('login-page')
-);
+const React = require('react')
+const ReactDOM = require('react-dom')
+const HomeContent = require('./home/homeContent')
+const Signup = require('./home')
+const Lobby = require('./lobby/lobby')
+const Layout = require('./layout')
+const Auth = require('./auth')
+const ReactRouter = require('react-router')
+const { Router, Route, hashHistory, IndexRoute } = ReactRouter
+
+const App = () => (
+  <Router history={hashHistory}>
+    <Route path='/' component={Layout}>
+      <IndexRoute component={HomeContent} />
+      <Route path='/signup' component={Auth} />
+      <Route path='/lobby' component={Lobby} />
+    </Route>
+  </Router>
+)
+
+ReactDOM.render(<App />, document.getElementById('login-page'))
