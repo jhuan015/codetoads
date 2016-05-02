8ddc2e1da5351b64b55267f8b01c0d22c0e2b81e
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-02 15:20:39 -0400

    add basic lobby components

---
+++ client/app/jsx/lobby/profile.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Profile extends React.Component {
+  render (){
+    return (
+      <div>
+        User Profile Template
+      </div>
+    )  
+  } 
+}
+
+module.exports = Profile
