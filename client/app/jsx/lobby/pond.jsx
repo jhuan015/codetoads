6ea0a1f5fe78ab2901eb37fbeb220fd55d20fd9e
anatomij <artjom.svjatickis@gmail.com>
2016-05-03 16:12:25 -0700

    (feat) Add pond.jsx UserList

---
+++ client/app/jsx/lobby/pond.jsx
@@ -0,0 +1,24 @@
+const React = require('react');
+const ReactRouter = require('react-router')
+var socket = io.connect();
+
+class UsersList extends React.Component {
+	render() {
+		return (
+			<div className='users'>
+				<h3> Online Users </h3>
+				<ul>
+					{
+						this.props.users.map((user, i) => {
+							return (
+								<li key={i}>
+									{user}
+								</li>
+							);
+						})
+					}
+				</ul>
+			</div>
+		);
+	}
+};
