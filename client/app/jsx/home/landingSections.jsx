f39dcf73f227931599510989a0507f72c08cbecd
Anthony Nourian <annourian@gmail.com>
2016-05-03 09:37:29 -0700

    (feat) Home Page components Add Landing static components for first layout

--- client/app/jsx/home/landingSections.jsx
+++ client/app/jsx/home/landingSections.jsx
@@ -1,33 +1,54 @@
+const React = require('react');
+const ReactRouter = require('react-router');
+const { Link } = ReactRouter;
 class LandingSections extends React.Component {
   constructor (){
     super();
     this.state = {};    
   }
-
   render (){
     return (
-      <div>
-        <section className='intro'>
-          Battle Toads
-        </section>
-        <section className='about'>
-          <div className='container'>
-            <div className='col-sm-5'>
-            </div>
-            <div className='col-sm-7'>
-              <h2>Test your prowess</h2>
-              <p>Become the master toad you always knew you could be.</p>
+      <div className='Landing'>
+        <section className='intro text-center'>
+          <div>
+            <h1>Code Toads</h1>
+            <h3>The pond isn't big enough for two</h3>
+            <hr className="intro-divider"></hr>
+            <div className="text-center">
+              <Link to='/Lobby' className='btn btn-primary btn-lg'>Battle Now</Link>              
             </div>
           </div>
         </section>
-        <section className=''>
-          <div className='col-sm-5'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+        <div className="content text-left">
+          <div className="container">
+              <div className="row">
+                  <div className="col-lg-6 col-sm-6">
+                      <hr className="content-heading-spacer"></hr>
+                      <div className="clearfix"></div>
+                      <h2 className="content-heading">Compete against the World</h2>
+                      <p className="lead">Tired of arguing who the better engineer is? Look no further. Compete against each other live with problems, compliments of codewars, to see which of you has the quickest wits to solve unknown prompts.</p>
+                  </div>
+                  <div className="col-lg-5 col-lg-offset-1 col-sm-6">
+                      <img className="content-img" src="app/img/codewars.png" alt="Codewars" />
+                  </div>
+              </div>
           </div>
-          <div className='col-sm-7'>
+        </div>
+        <div className="content-alt text-left">
+          <div className="container">
+              <div className="row">
+                  <div className="col-lg-5 col-sm-6">
+                    <img className="content-img" src="app/img/frogking.jpg" alt="King of the Pong" />                      
+                  </div>
+                  <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                      <hr className="content-heading-spacer"></hr>
+                      <div className="clearfix"></div>
+                      <h2 className="content-heading">Reign Surpreme</h2>
+                      <p className="lead">Dueling isn't just about competing, it's also a means to become a master at concepts you otherwise may not encounter in your daily life.</p>
+                  </div>
+              </div>
           </div>
-        </section>
-
+        </div>
       </div>
       );
   }
