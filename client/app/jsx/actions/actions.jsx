c947fe10e2cfeef4a045abb8f0ce3e5f9fef659a
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-04 18:08:02 -0400

    (feat) include submitattempt action creator

--- client/app/jsx/actions/actions.jsx
+++ client/app/jsx/actions/actions.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_PROMPT = 'FETCH_PROMPT';
+export const SUBMIT_ATTEMPT = 'SUBMIT_ATTEMPT';
 
 export function fetchPrompt() {
   const request = axios.post('/api/grabPrompt');
@@ -10,4 +11,13 @@ export function fetchPrompt() {
     type: FETCH_PROMPT,
     payload: request
   };
+}
+
+export function submitAttempt(ans) {
+  const request = axios.post('/api/submitAttempt', ans);
+  
+  return {
+    type: SUBMIT_ATTEMPT,
+    payload: request
+  }
 }
