9a2f8e70e3bac15df962615b4806d24c3c0e6d1f
Anthony Nourian <annourian@gmail.com>
2016-05-04 11:21:19 -0700

    (feat) Add profile components Add profilerow component Add profile component Add dummy data

--- client/app/jsx/lobby/profile.jsx
+++ client/app/jsx/lobby/profile.jsx
@@ -1,13 +1,32 @@
 const React = require('react');
+const ProfileRow = require('./profilerow');
 
 class Profile extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      auth_id: 1,
+      username: 'Batman',
+      winStreak: 10,
+      lostTo: 'Bane',
+      gamesPlayed: 500,
+      quits: 322,
+      fastest: '4 Minutes 30 Seconds'
+    };
+  }  
   render (){
+    var { username, winStreak, lostTo, gamesPlayed, quits, fastest } = this.state;
     return (
       <div>
-        User Profile Template
+        <h2>{username}</h2>
+        <ProfileRow title='Total Games Played' value={gamesPlayed}/>
+        <ProfileRow title='Highest Win Streak' value={winStreak}/>
+        <ProfileRow title='Last Wrecked By' value={lostTo}/>
+        <ProfileRow title='Rage Quits' value={quits}/>
+        <ProfileRow title='Fastest Time' value={fastest}/>        
       </div>
     )  
   } 
 }
 
-module.exports = Profile
+module.exports = Profile;
