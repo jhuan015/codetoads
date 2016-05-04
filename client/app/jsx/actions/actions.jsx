13cc225e7ef1ca1e6da17e37032f3ef79346c42e
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-04 15:29:46 -0400

    (refactor) update to send axios post

--- client/app/jsx/actions/actions.jsx
+++ client/app/jsx/actions/actions.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const FETCH_PROMPT = 'FETCH_PROMPT';
 
 export function fetchPrompt() {
-  const request = axios.get('/api/grabPrompt');
+  const request = axios.post('/api/grabPrompt');
+  // console.log(request)
 
   return {
     type: FETCH_PROMPT,
