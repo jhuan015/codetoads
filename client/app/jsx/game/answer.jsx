6e752a2966fa0da3aa8eca219c4971d16458d921
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-04 18:10:02 -0400

    (feat) set up answer component with props

--- client/app/jsx/game/answer.jsx
+++ client/app/jsx/game/answer.jsx
@@ -1,10 +1,30 @@
 const React = require('react');
 
 class Answer extends React.Component {
+  constructor (){
+    super()
+    this.state = { term: '' };
+  }
+  
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      term: nextProps.session.setup
+    });
+  }
+  
+  _onInputChange(term) {
+    this.setState({term});
+  }
+  
   render (){
     return (
       <div>
-        Answer goes here.
+        <p>{JSON.stringify(this.props.session)}</p>
+        <textArea
+          value={this.state.term}
+          onChange={event => this._onInputChange(event.target.value)}
+         />
+        <button>Submit</button>
       </div>
     )  
   } 
