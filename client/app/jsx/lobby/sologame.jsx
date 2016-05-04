f50e82c968eb6a1fc7d98e218490c3682d310b27
Anthony Nourian <annourian@gmail.com>
2016-05-04 11:22:10 -0700

    (feat) Solo tab Component Add solo component Add difficulty selection component

---
+++ client/app/jsx/lobby/sologame.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+const {Button} = require('react-bootstrap');
+const DiffButton = require('./difficultybutton');
+
+class Sologame extends React.Component {
+  render(){
+    return (
+      <div className='text-center'>
+        <div className='clearfix solo-dropdown'>
+          <DiffButton/>
+        </div>
+        <Button bsStyle='primary' bsSize='large'>Start</Button>
+        
+      </div>
+      );
+  }
+}
+
+module.exports = Sologame;
