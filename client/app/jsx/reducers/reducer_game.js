9649eda678e956d0b7300573288ac819b5e6fb3f
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-05 18:04:03 -0400

    (fix) include setup state property for reducer

--- client/app/jsx/reducers/reducer_game.js
+++ client/app/jsx/reducers/reducer_game.js
@@ -1,13 +1,14 @@
  import { FETCH_PROMPT, SUBMIT_ATTEMPT } from '../actions/actions';
 
-const INITIAL_STATE = { prompt: {}, attempt: { ouput: [], reason: ''} };
+const INITIAL_STATE = { prompt: {}, attempt: { ouput: [], reason: ''} , setup: "" };
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_PROMPT:
-    return { prompt: action.payload.data, attempt: state.attempt };
+    return { prompt: action.payload.data, attempt: state.attempt, setup: action.payload.data.session.setup };
   case SUBMIT_ATTEMPT:
-    return { prompt: state.prompt, attempt: action.payload.data };
+    console.log(typeof JSON.parse(action.payload.data.response));
+    return { prompt: state.prompt, attempt: JSON.parse(action.payload.data.response), setup: action.payload.data.setup };
   default:
     return state;
   }
