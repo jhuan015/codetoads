431139f569a6f2c66af8cb55c64fde65e5437f19
Anthony Nourian <annourian@gmail.com>
2016-05-05 11:51:18 -0700

    (style) Add testresult styles refactor result as innerHTML

--- client/app/jsx/game/testResults.jsx
+++ client/app/jsx/game/testResults.jsx
@@ -4,15 +4,18 @@ class TestResults extends React.Component {
   constructor () {
     super()
   }
+  /*rawMarkup() {   
+   return {__html: this.props.reason};
+ }*/
   render (){
     if(!this.props.output || !this.props.reason){
       return (
-        <div className='testResults'>Make an attempt!</div>  
+        <div className='testInitial'>Make an attempt!</div>  
       )
     }
     return (
       <div className='testResults'>
-        <div className='testResults__title'>{this.props.output}</div>
+        <div dangerouslySetInnerHTML={{__html:this.props.output}} className='testResults__title'></div>
         <div className='testResults__body'>{this.props.reason}</div>
       </div>
     )  
