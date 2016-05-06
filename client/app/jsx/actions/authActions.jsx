46e6c81bcb70585e386ffc76d3c62a38824598ea
anatomij <artjom.svjatickis@gmail.com>
2016-05-06 13:54:48 -0700

    (feat) Add logout actions in authActions

--- client/app/jsx/actions/authActions.jsx
+++ client/app/jsx/actions/authActions.jsx
@@ -43,3 +43,32 @@ export function login() {
     });
   };
 }
+
+// Logout Actions
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
+
+function requestLogout() {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true,
+  };
+}
+
+function receiveLogout() {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false,
+  };
+}
+// Logs User out and remove token from localStorage
+export function logoutUser() {
+  return dispatch => {
+    dispatch(requestLogout());
+    localStorage.removeItem('id_token');
+    dispatch(receiveLogout());
+  };
+}
