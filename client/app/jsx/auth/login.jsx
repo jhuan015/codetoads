6dd4907065024a452ac876612410deb97038efaa
anatomij <artjom.svjatickis@gmail.com>
2016-05-06 14:45:16 -0700

    (feat) Add login component

---
+++ client/app/jsx/auth/login.jsx
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Login extends Component {
+  render() {
+    const { errorMessage } = this.props;
+
+    return (
+      <a className="login-btn" onClick={(e) => this.handleClick(e)}>
+        Login
+      </a>
+    );
+  }
+
+  handleClick(e) {
+    this.props.onLoginClick();
+  }
+};
+
+Login.propTypes = {
+  onLoginClick: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string,
+};
