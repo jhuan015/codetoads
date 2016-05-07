47ddfbbe36720dcab00962c9b5ac68ce7b210f69
anatomij <artjom.svjatickis@gmail.com>
2016-05-06 20:53:31 -0700

    (feat) Add logout component for Auth0

---
+++ client/app/jsx/auth/logout.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Logout extends Component {
+  render() {
+    const { onLogoutClick } = this.props;
+
+    return (
+        <a href="/" onClick={() => onLogoutClick()}>
+          Logout
+        </a>
+    );
+  }
+};
+
+Logout.propTypes = {
+  onLogoutClick: PropTypes.func.isRequired,
+};
