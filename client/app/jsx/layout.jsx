7debda8c23295cdc4ce1ee32a84ba12c5699331d
anatomij <artjom.svjatickis@gmail.com>
2016-05-06 20:43:42 -0700

    (feat) Add Navbar Authentication

--- client/app/jsx/layout.jsx
+++ client/app/jsx/layout.jsx
@@ -1,20 +1,23 @@
-const React = require('react')
-const ReactRouter = require('react-router')
-const Nav = require('./navbar')
-const Footer = require('./footer')
-const { Link } = ReactRouter
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Nav from './navbar';
+const { Link } = require('react-router');
+const Footer = require('./footer');
 
 class Layout extends React.Component {
-  constructor (){
-    super();
-    this.state = {
-      status: 'Signup'
-    };
-  }
+
   render () {
+    const { dispatch, isAuthenticated, errorMessage } = this.props;
+    console.log(this.props);
+    console.log(dispatch);
+    console.log(isAuthenticated);
     return (
       <div className="clearfix">
-        <Nav status={this.state.status} />
+      <Nav
+        isAuthenticated={isAuthenticated}
+        errorMessage={errorMessage}
+        dispatch={dispatch}
+      />
         <div className="body-wrap">
           {this.props.children}
         </div>
