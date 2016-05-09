dba7ca13106601eecc233b457d9f46671e93c98b
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-09 18:27:05 -0400

    (refactor) create action to get next prompt

--- client/app/jsx/actions/actions.jsx
+++ client/app/jsx/actions/actions.jsx
@@ -4,6 +4,7 @@ export const FETCH_PROMPTS = 'FETCH_PROMPTS';
 export const SUBMIT_ATTEMPT = 'SUBMIT_ATTEMPT';
 export const CREATE_DIFFICULTY = 'CREATE_DIFFICULTY';
 export const SAVE_USER = 'SAVE_USER';
+export const NEXT_PROMPT = 'NEXT_PROMPT';
 
 export function fetchPrompts(difficulty, numPrompt) {
   const request = axios.post('/api/makeGame', { "difficulty": 'easy', "numPrompt": 2});
@@ -14,22 +15,28 @@ export function fetchPrompts(difficulty, numPrompt) {
 }
 export function submitAttempt(ans) {
   const request =  axios.post('/api/submitAttempt', ans)
-
   return {
     type: SUBMIT_ATTEMPT,
     payload: request
-  }
+  };
 }
 export function createDifficulty(difficulty) {
   return {
     type: CREATE_DIFFICULTY,
     difficulty: difficulty
-  }
+  };
 }
 export function saveUser(profile) {
   const request = axios.post('/api/saveUser', profile);
   return {
     type: SAVE_USER,
     payload: request
-  }
+  };
 }
+export function nextPrompt(index) {
+  return {
+    type: NEXT_PROMPT,
+    index: index
+  };
+}
+
