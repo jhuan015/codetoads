6ea6f85a660901f563440901802335e66d5bfccc
Jonathan Huang <jhuan015@ucr.edu>
2016-05-10 22:27:02 -0700

    (feat) Update to use username
    Also add basic tracking of progress.

--- server/socket.js
+++ server/socket.js
@@ -11,18 +11,6 @@ var userNames = (function () {
     }
   };
 
-  // find the lowest unused "guest" name and claim it
-  var getGuestName = function () {
-    var name,
-      nextUserId = 1;
-
-    do {
-      name = 'Guest ' + nextUserId;
-      nextUserId += 1;
-    } while (!claim(name));
-
-    return name;
-  };
 
   // serialize claimed names as an array
   var get = function () {
@@ -43,61 +31,69 @@ var userNames = (function () {
   return {
     claim: claim,
     free: free,
-    get: get,
-    getGuestName: getGuestName
+    get: get
   };
 }());
 
 // export function for listening to the socket
+var users = [];
+var people = {};
+var gameStatus = {};
+
 module.exports = function (socket) {
-  var name = userNames.getGuestName();
+  socket.name = socket.handshake.query.user;
   var room = socket.handshake.query.chatroom;
   socket.join(room);
+  people[socket.name] = socket.name;
+  gameStatus[socket.name] = {
+    name: socket.name,
+    goal: 0
+  };
+  users.push(socket.name);
+
+
   console.log("joined room: " + room);
   // send the new user their name and a list of users
-  socket.to(room).emit('init', {
-    name: name,
-    users: userNames.get()
+  this.to(room).emit('init', {
+    name: socket.name,
+    users: users
+ //   users: people
   });
 
   // notify other clients that a new user has joined
-  socket.to(room).emit('user:join', {
-    name: name
+  this.to(room).emit('user:join', {
+    name: socket.name
   });
 
   // broadcast a user's message to other users
   socket.on('send:message', function (data) {
     console.log('send message in room: ' + room);
-    socket.to(room).emit('send:message', {
-      user: name,
+    this.to(room).emit('send:message', {
+      user: socket.name,
       text: data.text
     });
   });
 
-  // validate a user's name change, and broadcast it on success
-  socket.on('change:name', function (data, fn) {
-    if (userNames.claim(data.name)) {
-      var oldName = name;
-      userNames.free(oldName);
 
-      name = data.name;
+  socket.on('person:won', function (data, fn) {
+    console.log(data.test + ' has won!');
+  });
 
-      socket.to(room).emit('change:name', {
-        oldName: oldName,
-        newName: name
-      });
-
-      fn(true);
-    } else {
-      fn(false);
-    }
+  socket.on('person:passed', function (data, fn) {
+    console.log('pass ' + data.name);
+    console.log(room);
+    gameStatus[data.name].goal++;
+    this.to(room).emit('update:game', {
+      name: data.name,
+      goal: gameStatus[data.name].goal
+    });
   });
 
   // clean up when a user leaves, and broadcast it to other users
   socket.on('disconnect', function () {
-    socket.to(room).emit('user:left', {
-      name: name
+    this.to(room).emit('user:left', {
+      name: socket.name
     });
-    userNames.free(name);
+    userNames.free(socket.name);
   });
 };
