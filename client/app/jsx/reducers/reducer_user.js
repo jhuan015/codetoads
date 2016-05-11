ed935cce695255f036d10d82ce6673861b60d3dc
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-11 17:51:39 -0400

    (fix) Update user based on payload.data

--- client/app/jsx/reducers/reducer_user.js
+++ client/app/jsx/reducers/reducer_user.js
@@ -20,18 +20,18 @@ export default function (state = INIT_STATE, action){
   switch (action.type){
     case GET_USER_INFO:
       return {
-        id: action.payload.id || '',
-        user_id: action.payload.user_id || '',
-        username: action.payload.username || '',
-        firstname: action.payload.firstname || '',
-        lastname: action.payload.lastname || '',
-        winStreak: action.payload.winStreak || 0,
-        lostTo: action.payload.lostTo || '',
-        gamesPlayed: action.payload.gamesPlayed || 0,
-        quits: action.payload.quits || 0,
-        fastest: action.payload.fastest || 0,
-        email: action.payload.email || '',
-        picture: action.payload.picture || ''
+        id: action.payload.data.id || '',
+        user_id: action.payload.data.user_id || '',
+        username: action.payload.data.username || '',
+        firstname: action.payload.data.firstname || '',
+        lastname: action.payload.data.lastname || '',
+        winStreak: action.payload.data.winStreak || 0,
+        lostTo: action.payload.data.lostTo || "You haven't lost yet",
+        gamesPlayed: action.payload.data.gamesPlayed || 0,
+        quits: action.payload.data.quits || 0,
+        fastest: action.payload.data.fastest || 0,
+        email: action.payload.data.email || '',
+        picture: action.payload.data.picture || ''
       };
     default:
     return state;
