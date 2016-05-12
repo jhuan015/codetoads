a16394d01d7d4ab76f3c7eda7059dbf91e1891c5
Jonathan Huang <jhuan015@ucr.edu>
2016-05-12 00:35:07 -0700

    (test) Add Chai dependency and additional API tests

--- spec/server.js
+++ spec/server.js
@@ -1,7 +1,8 @@
 var server = require('../server/server');
 var request = require('supertest')(server);
+var assert = require('chai').assert;
 
-describe('Server API Calls', function () {
+describe('Main Page Calls', function () {
   it('responds to /', function testSlash(done) {
   request
     .get('/')
@@ -14,12 +15,76 @@ describe('Server API Calls', function () {
       .expect(404, done);
   });
 
-  it('grabs a prompt', function (done) {
-    var data = { challenge: 'easy' };
+});
+
+describe('makeGame calls', function() {
+  var data = {
+    difficulty: 'easy',
+    numPrompt: 2
+  };
+
+  it('make a game with 2 prompts', function (done) {
     request
-      .post('api/grabPrompt')
+      .post('/api/makeGame')
         .send(data)
         .expect(200)
-        .end(done);
+        .end(function (err, res) {
+          assert.equal(res.body.length, 2);
+          done();
+        });
   });
+
+  it('make a game with 3 prompts', function (done) {
+    var data = {
+      difficulty: 'easy',
+      numPrompt: 3
+    };
+    request
+      .post('/api/makeGame')
+        .send(data)
+        .expect(200)
+        .end(function (err, res) {
+          assert.equal(res.body.length, 3);
+          done();
+        });
+  });
+
+  it('grab a prompt with code', function (done) {
+    request
+      .post('/api/makeGame')
+        .send(data)
+        .expect(200)
+        .end(function (err, res) {
+          assert.property(res.body[0].session, 'code', "has code property");
+          assert.property(res.body[0].session, 'setup', "has setup property");
+          done();
+        });
+  });
+
 });
+
+describe('submitAttempt calls', function () {
+  it('should allow submission attempts', function (done) {
+    this.timeout(5000);
+
+    var attempt = {
+      project_id: '5727dcf97fc662c6970009e2',
+      solution_id: '5727dcf90838ffce0f000918',
+      code: 'function greet(){return \'hello world!\'}'
+    }
+    request
+      .post('/api/submitAttempt')
+        .send(attempt)
+        .expect(200)
+        .end(function (err, res) {
+          assert.property(res.body, 'response', 'has response property');
+          assert.property(res.body, 'setup', 'has setup property');
+          // console.log(JSON.parse(res.body.response).success);
+          // console.log(JSON.parse(res.body.response));
+          // assert.isTrue(JSON.parse(res.body.response).success);
+          done();
+        });
+  });
+
+});
+
