e3dda844369415700d699f2970ecf6ba6e835c99
Jonathan Huang <jhuan015@ucr.edu>
2016-05-14 17:52:55 -0700

    (feat) Update with SweetAlerts and to use props

--- client/app/jsx/game/multichoice/multichoice.jsx
+++ client/app/jsx/game/multichoice/multichoice.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import SweetAlert from 'sweetalert-react';
 
 class MultiChoice extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      session: {
-        question: 'What kind of scoping does JavaScript use?',
-        choices: ['Literal', 'Lexical', 'Segmental', 'Sequential'],
-        answer: 'b',
-        explanation: 'Like most modern programming languages, JavaScript uses lexical scoping. This means that functions are executed using the variable scope that was in effect when they were defined, not the variable scope that is in effect when they are invoked.',
-        type: 'multichoice'
-      },
-      selected: ''
+      selected: '',
+      show: false
     };
   }
 
   _submit () {
     if (this.state.selected === this.props.session.answer) {
       console.log('true');
-      return true;
+      this.setState({
+        show: true
+      })
     } else {
       console.log('false');
       return false;
@@ -38,18 +35,34 @@ class MultiChoice extends React.Component {
   render () {
     return (
       <div>
-      <p>{this.props.session.question}</p>
-      <ul className="answers">
-      <input className='with-gap' type="radio" name="q1" value="a" id="a1" onClick={this._selected.bind(this, 'a')} />
-        <label htmlFor="a1">{this.props.session.choices[0]}</label><br />
-      <input className='with-gap' type="radio" name="q1" value="b" id="a2" onClick={this._selected.bind(this, 'b')} />
-        <label htmlFor="a2">{this.props.session.choices[1]}</label><br />
-      <input className='with-gap' type="radio" name="q1" value="c" id="a3" onClick={this._selected.bind(this, 'c')} />
-        <label htmlFor="a3">{this.props.session.choices[2]}</label><br />
-      <input className='with-gap' type="radio" name="q1" value="d" id="a4" onClick={this._selected.bind(this, 'd')} />
-        <label htmlFor="a4">{this.props.session.choices[3]}</label><br />
-      </ul>
-      <Button bsStyle='primary' bsSize='large' onClick={this._submit.bind(this)}>Submit</Button>
+         <SweetAlert
+          show={this.state.show && !this.props.complete}
+          imageUrl= "app/img/ironfrog.gif"
+          imageSize= '250x250'
+          title="Success!"
+          text={this.props.session.explanation}
+          onConfirm={() => this.setState({show: false})}
+        />
+        <SweetAlert
+          show={this.state.show && this.props.complete}
+          imageUrl= "app/img/jumping_frog.gif"
+          imageSize= '250x250'
+          title="Great job!"
+          text="You've finished all the prompts."
+          onConfirm={() => this.setState({show: false})}
+        />
+        <p>{this.props.session.question}</p>
+        <ul className="answers">
+        <input className='with-gap' type="radio" name="q1" value="a" id="a1" onClick={this._selected.bind(this, 'a')} />
+          <label htmlFor="a1">{this.props.session.choices[0]}</label><br />
+        <input className='with-gap' type="radio" name="q1" value="b" id="a2" onClick={this._selected.bind(this, 'b')} />
+          <label htmlFor="a2">{this.props.session.choices[1]}</label><br />
+        <input className='with-gap' type="radio" name="q1" value="c" id="a3" onClick={this._selected.bind(this, 'c')} />
+          <label htmlFor="a3">{this.props.session.choices[2]}</label><br />
+        <input className='with-gap' type="radio" name="q1" value="d" id="a4" onClick={this._selected.bind(this, 'd')} />
+          <label htmlFor="a4">{this.props.session.choices[3]}</label><br />
+        </ul>
+        <Button bsStyle='primary' bsSize='large' onClick={this._submit.bind(this)}>Submit</Button>
       </div>
     )
   }
