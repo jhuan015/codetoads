15ddf4c270bab59b0e07b124b626cb5cbabe2a87
Jonathan Huang <jhuan015@ucr.edu>
2016-05-14 17:32:40 -0700

    (feat) Change to use props

--- client/app/jsx/game/multichoice/multichoice.jsx
+++ client/app/jsx/game/multichoice/multichoice.jsx
@@ -6,8 +6,6 @@ class MultiChoice extends React.Component {
     super()
 
     this.state = {
-      name: 'Multiple Choice Challenge!',
-      description: 'Answer the question by submitting the correct answer.',
       session: {
         question: 'What kind of scoping does JavaScript use?',
         choices: ['Literal', 'Lexical', 'Segmental', 'Sequential'],
@@ -20,7 +18,7 @@ class MultiChoice extends React.Component {
   }
 
   _submit () {
-    if (this.state.selected === this.state.session.answer) {
+    if (this.state.selected === this.props.session.answer) {
       console.log('true');
       return true;
     } else {
@@ -40,16 +38,16 @@ class MultiChoice extends React.Component {
   render () {
     return (
       <div>
-      <p>{this.state.session.description}</p>
+      <p>{this.props.session.question}</p>
       <ul className="answers">
       <input className='with-gap' type="radio" name="q1" value="a" id="a1" onClick={this._selected.bind(this, 'a')} />
-        <label htmlFor="a1">{this.state.session.choices[0]}</label><br />
+        <label htmlFor="a1">{this.props.session.choices[0]}</label><br />
       <input className='with-gap' type="radio" name="q1" value="b" id="a2" onClick={this._selected.bind(this, 'b')} />
-        <label htmlFor="a2">{this.state.session.choices[1]}</label><br />
+        <label htmlFor="a2">{this.props.session.choices[1]}</label><br />
       <input className='with-gap' type="radio" name="q1" value="c" id="a3" onClick={this._selected.bind(this, 'c')} />
-        <label htmlFor="a3">{this.state.session.choices[2]}</label><br />
+        <label htmlFor="a3">{this.props.session.choices[2]}</label><br />
       <input className='with-gap' type="radio" name="q1" value="d" id="a4" onClick={this._selected.bind(this, 'd')} />
-        <label htmlFor="a4">{this.state.session.choices[3]}</label><br />
+        <label htmlFor="a4">{this.props.session.choices[3]}</label><br />
       </ul>
       <Button bsStyle='primary' bsSize='large' onClick={this._submit.bind(this)}>Submit</Button>
       </div>
