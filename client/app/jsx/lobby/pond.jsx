7220deae1f08a62038d42e50722b197f33de6fc9
Jonathan Huang <jhuan015@ucr.edu>
2016-05-15 17:51:45 -0700

    (fix) Fix spacing with chat messages

--- client/app/jsx/lobby/pond.jsx
+++ client/app/jsx/lobby/pond.jsx
@@ -27,7 +27,7 @@ class Message extends React.Component {
 	render() {
 		return (
 			<div className="message">
-				<strong>{this.props.user} :</strong>
+				<strong>{this.props.user}: </strong>
 				<span>{this.props.text}</span>
 			</div>
 		);
