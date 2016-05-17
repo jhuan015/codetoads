09e1a6585c1532303363e6939ded6f1668e36deb
anatomij <artjom.svjatickis@gmail.com>
2016-05-16 19:34:00 -0700

    (fix) Fix winner emit and passed

--- server/socket.js
+++ server/socket.js
@@ -98,20 +98,20 @@ module.exports = function (socket) {
     });
   });
 
-
+  //person won
   socket.on('person:won', function (data, fn) {
     console.log(data.test + ' has won!');
   });
 
-
+  //person Passed
   socket.on('person:passed', function(data) {
-    for (var i = 0; i < gameStatus[room].player.length; i++) {
-      if (data.name === gameStatus[room].player[i].name) {
-        gameStatus[room].player[i].current++;
+    for (var i = 0; i < roomStatus[room].player.length; i++) {
+      if (data.name === roomStatus[room].player[i].name) {
+        roomStatus[room].player[i].current++;
         //emit to everyone and socket that sent it
-        this.to(room).emit('update:game', gameStatus[room]);
-        socket.emit('update:game', gameStatus[room]);
-        if (gameStatus[room].player[i].current === gameStatus[room].goal) {
+        this.to(room).emit('update:game', roomStatus[room]);
+        socket.emit('update:game', roomStatus[room]);
+        if (roomStatus[room].player[i].current === roomStatus[room].goal) {
           this.to(room).emit('winner', {
             winner: data.name
           });
