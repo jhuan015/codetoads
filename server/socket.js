24867f44256bf363dc02ca05fb4cca5db2c350ec
anatomij <artjom.svjatickis@gmail.com>
2016-05-16 19:32:52 -0700

    (fix) Change to roomStatus

--- server/socket.js
+++ server/socket.js
@@ -1,87 +1,92 @@
+var passwordHash = require('password-hash');
 // export function for listening to the socket
-var gameStatus = {};
+var roomStatus = {};
 var socketRooms = {};
 
 module.exports = function (socket) {
+
   socket.name = socket.handshake.query.user;
   var room = socket.handshake.query.chatroom;
   var type = socket.handshake.query.joinType;
-  var password = socket.handshake.query.passWord;
-  
+  var password = socket.handshake.query.password;
+  var difficulty = socket.handshake.query.difficulty;
+  console.log(JSON.stringify(socket.handshake.query));
+
   socket.join(room);
-  
-  if (!gameStatus[room]){
-    gameStatus[room] = {
+  if (!roomStatus[room]){
+    roomStatus[room] = {
       player:[],
-      goal:5,
-      started:false
+      amount:0,
+      started:false,
+      password:password
     };
   }
-  var found = false;
-  for (var i = 0; i < gameStatus[room].player.length; i++) {
-    if (gameStatus[room].player[i].name === socket.name){
-      found = true;
+  if (socket.name){
+    var nameCheck = false;
+    roomStatus[room].player.forEach(function(value, index){
+      if(value.name === socket.name){
+        nameCheck = true;
+      }
+    });
+    if (!nameCheck){
+      roomStatus[room].player.push({name:socket.name});
     }
   }
-  if (!found){
-    gameStatus[room].player.push({
-        name: socket.name,
-        current: 0
-      });
-  } else {
-    //send game data for reconnect
-  }
-
 
   //check game from lobby before redirect
   socket.on('gamecheck:status', function (data) {
-    if (data.check){
-      socket.emit('gamecheckGame:complete', {check:true,rooms:socketRooms});
-    }
+    console.log('password:');
+    console.log( data.password);
     var status = {type:data.type, value:true, passCheck:true,full:false};
     if (data.type === 'create'){
-      if (!socketRooms[data.room]){
-        socketRooms[data.room] = {players:[], creator:data.user, password:data.password};
-        socketRooms[data.room].players.push(data.user);
+      if (roomStatus[data.room]){
+        status.value = false;
       } else {
-        status.value =false;
-      }      
+        var pass = passwordHash.generate(data.password);
+        status.password = pass;
+      }
     } else if (data.type === 'join'){
-      if (socketRooms[data.room]){
-        if (socketRooms[data.room].password !== data.password){
-          status.passCheck = false;          
+      if (roomStatus[data.room]){
+        if (!passwordHash.verify(data.password, roomStatus[data.room].password)){
+          console.log('hashedPass:');
+          console.log(roomStatus[data.room].password);
+          status.passCheck = false;
         } else {
-          if (socketRooms[data.room].players.indexOf(data.user) < 0){
-            if (socketRooms[data.room].players.length === 4){
-              status.full = true;
+          var found = false;
+          for (var a = 0; a < roomStatus[data.room].player.length; a++){
+            if (roomStatus[data.room].player[a].name === data.user){
+              found = true;
             }
+          }
+          if (!found && roomStatus[data.room].player.length > 3 ){
+            status.full = true;
+            status.value = false;
+          } else if(!found && roomStatus[data.room].player.length < 4) {
+            status.password = roomStatus[data.room].password;
           }
         }
       } else {
         status.value = false;
-      }       
-    } 
+      }
+    }
     socket.emit('gamecheck:complete',status);
-  }); 
-
-  this.to(room).emit('update:game', gameStatus[room])
-  console.log(gameStatus[room]);
+  });
 
   console.log("joined room: " + room);
   // send the new user their name and a list of users
   this.to(room).emit('init', {
     name: socket.name,
-    users: gameStatus[room].player
+    users: roomStatus[room].player
   });
   socket.to(room).emit('init', {
     name: socket.name,
-    users: gameStatus[room].player
+    users: roomStatus[room].player
   });
 
   // notify other clients that a new user has joined
   this.to(room).emit('user:join', {
-    name: gameStatus[room].player[gameStatus[room].player.length - 1].name,
-    users: gameStatus[room].player
+    name: roomStatus[room].player[roomStatus[room].player.length - 1].name,
+    users: roomStatus[room].player
   });
 
   // broadcast a user's message to other users
