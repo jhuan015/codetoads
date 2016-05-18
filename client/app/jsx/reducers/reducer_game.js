734563ff78add85d55f2c51349d6830ac2671efc
anatomij <artjom.svjatickis@gmail.com>
2016-05-18 12:37:14 -0700

    (feat) Add users to the state and UPDATE and SAVE cases

--- client/app/jsx/reducers/reducer_game.js
+++ client/app/jsx/reducers/reducer_game.js
@@ -1,17 +1,20 @@
- import { FETCH_PROMPTS, SUBMIT_ATTEMPT, CLOSE_ALERT, NEXT_PROMPT, CHEAT, UPDATE_PROMPTS, START_GAME } from '../actions/actions';
+ import { FETCH_PROMPTS, SUBMIT_ATTEMPT, CLOSE_ALERT, NEXT_PROMPT, CHEAT, UPDATE_PROMPTS, START_GAME, SAVE_GAME, UPDATE_USERS } from '../actions/actions';
 
-const INITIAL_STATE = { prompts: [], attempt: { ouput: [], reason: ''}, passed: false, index: 0, alert: false, started:false};
+const INITIAL_STATE = { prompts: [], attempt: { ouput: [], reason: ''}, passed: false, index: 0, alert: false, started:false, users: []};
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
   case FETCH_PROMPTS:
+  console.log('inside fetch prompts');
+  console.log(action.payload);
     return {
       prompts: action.payload.data,
       attempt: state.attempt,
       passed: false,
       index: 0,
       alert: state.alert,
-      started:false
+      started: false,
+      users: state.users
     };
   case SUBMIT_ATTEMPT:
     return {
@@ -20,7 +23,7 @@ export default function(state = INITIAL_STATE, action) {
       passed: (JSON.parse(action.payload.data.response)).passed,
       index: state.index,
       alert: true,
-      started:state.started
+      users: state.users
     };
   case CLOSE_ALERT:
     return {
@@ -29,7 +32,8 @@ export default function(state = INITIAL_STATE, action) {
       passed: state.passed,
       index: state.index,
       alert: false,
-      started:state.started
+      started: state.started,
+      users: state.users
     }
   case NEXT_PROMPT:
     return {
@@ -38,7 +42,8 @@ export default function(state = INITIAL_STATE, action) {
       passed: false,
       index: action.index,
       alert: state.alert,
-      started:state.started
+      started: state.started,
+      users: state.users
     }
   case CHEAT:
     return {
@@ -47,7 +52,8 @@ export default function(state = INITIAL_STATE, action) {
       passed: true,
       index: state.index,
       alert: true,
-      started:state.started
+      started: state.started,
+      users: state.users
     }
   case UPDATE_PROMPTS:
     return {
@@ -56,7 +62,8 @@ export default function(state = INITIAL_STATE, action) {
       passed: state.passed,
       index: state.index,
       alert: state.alert,
-      started:action.payload.started
+      started: action.payload.started,
+      users: state.users
     }
   case START_GAME:
     return {
@@ -65,7 +72,27 @@ export default function(state = INITIAL_STATE, action) {
       passed: state.passed,
       index: state.index,
       alert: state.alert,
-      started:true
+      started: true,
+      users: state.users
+    }
+  case SAVE_GAME:
+    return {
+      prompts: state.prompts,
+      attempt: state.attempt,
+      passed: state.passed,
+      index: state.index,
+      alert: state.alert,
+      started: state.started,
+      users: state.users
+    }
+  case UPDATE_USERS:
+    return {
+      prompts: state.prompts,
+      attempt: state.attempt,
+      passed: state.passed,
+      index: state.index,
+      alert: state.alert,
+      users: action.users
     }
   default:
     return state;
