342db06346b295e5c20628fe0e055342f32386b0
RobinGiannattasio <Robin.Giannattasio@gmail.com>
2016-05-18 17:06:41 -0400

    (fix) Force refresh on logout when jwt expires

--- client/app/jsx/lobby/lobby.jsx
+++ client/app/jsx/lobby/lobby.jsx
@@ -10,6 +10,7 @@ class Lobby extends React.Component {
   render () {
     if(this.props.error){
       this.props.logoutUser();
+      window.location.reload;
     }
     return (
       <div className='lobby row'>
