787d46f85ac584f25e124f968dc152a9b75c2e9b
anatomij <artjom.svjatickis@gmail.com>
2016-05-18 12:35:20 -0700

    (feat) Completed prompts emit and call saveGame

--- client/app/jsx/game/race.jsx
+++ client/app/jsx/game/race.jsx
@@ -1,29 +1,37 @@
 const React = require('react');
 import Bar from './bar';
+import Timer from './timer';
 
 class Race extends React.Component {
   constructor (){
     super();
-    this.state= {game:{}};
+    this.state= {
+      game:{}
+    }
   }
 
   componentDidMount() {
     socket.on('update:game', this._updateGame.bind(this));
     socket.on('winner', this._winner.bind(this));
+    socket.on('completed', this._completed.bind(this));
   }
 
   _winner(data) {
     console.log(data.winner + ' WON!');
   }
-
+  _completed(){
+    console.log(data.completed + 'Completed the game!!!!!!!!!!!');
+  }
   _updateGame(data) {
     this.setState({game:data});
+    this.props.saveGame(this.state.game);
   }
 
 
   render (){
     return (
       <div className='race'>
+      <Timer/>
       {this.state.game.player &&
             this.state.game.player.map((user, i) => {
               return (
@@ -31,9 +39,9 @@ class Race extends React.Component {
                   <div className="col-sm-2">
                   {user.name}
                   </div>
-                  <div className="col-sm-10">                      
+                  <div className="col-sm-10">
                     <div className={'player'+i}>
-                     <Bar amount={user.current} />
+                     <Bar amount={user.progress} />
                     </div>
                   </div>
                 </div>
